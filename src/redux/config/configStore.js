db795017771e8e98dd9c8f16c8e1a660a251ec0d
Choi Yoonseo <choiys1103@naver.com>
2023-06-22 15:04:46 +0900

    Merge: develop -> main

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,9 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 import modal from "../modules/modal";
-import button from "../modules/button";
 
 const rootReducer = combineReducers({
   modal,
-  button,
 });
 const store = createStore(rootReducer);
 
